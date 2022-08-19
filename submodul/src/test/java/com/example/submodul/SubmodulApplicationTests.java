10efacb34abe48e7d351aef4631dd8f2c10361da
gaochao <gaochao1996@163.com>
2022-08-19 19:12:04 +0800

    添加子模块

---
+++ submodul/src/test/java/com/example/submodul/SubmodulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.submodul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmodulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
